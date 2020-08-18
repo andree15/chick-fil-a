786447e4413db76d9d493053e97eaa37542d29a6
Andy Edstrom <andyedstrom115@gmail.com>
2020-08-18 12:06:33 -0400

    added timers to the rest of the buttons on all machines

--- js/app.js
+++ js/app.js
@@ -43,3 +43,115 @@ document.getElementById("fillet4").addEventListener("click", function() {
         display = document.querySelector("#current4");
         startTimer(fillet, display);
 });
+
+
+// Nuggets
+
+
+document.getElementById("nug1").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current1");
+        startTimer(fillet, display);
+});
+
+document.getElementById("nug2").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current2");
+        startTimer(fillet, display);
+});
+
+document.getElementById("nug3").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current3");
+        startTimer(fillet, display);
+});
+
+document.getElementById("nug4").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current4");
+        startTimer(fillet, display);
+});
+
+
+// Strips
+
+
+document.getElementById("strip1").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current1");
+        startTimer(fillet, display);
+});
+
+document.getElementById("strip2").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current2");
+        startTimer(fillet, display);
+});
+
+document.getElementById("strip3").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current3");
+        startTimer(fillet, display);
+});
+
+document.getElementById("strip4").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current4");
+        startTimer(fillet, display);
+});
+
+
+// Breakfast Fillets
+
+
+document.getElementById("brkfillet1").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current1");
+        startTimer(fillet, display);
+});
+
+document.getElementById("brkfillet2").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current2");
+        startTimer(fillet, display);
+});
+
+document.getElementById("brkfillet3").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current3");
+        startTimer(fillet, display);
+});
+
+document.getElementById("brkfillet4").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current4");
+        startTimer(fillet, display);
+});
+
+
+// :45 Seconds
+
+
+document.getElementById("45s1").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current1");
+        startTimer(fillet, display);
+});
+
+document.getElementById("45s2").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current2");
+        startTimer(fillet, display);
+});
+
+document.getElementById("45s3").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current3");
+        startTimer(fillet, display);
+});
+
+document.getElementById("45s4").addEventListener("click", function() {
+    var fillet = 60 * 5,
+        display = document.querySelector("#current4");
+        startTimer(fillet, display);
+});
